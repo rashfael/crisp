0a5475979b4d00556eb49008be87f820ee63383f
rashfael <rashfael@isobeef.org>
2016-07-09 16:58:40 +0200

    add prod build chain

--- web-client/build/webpack.base.conf.js
+++ web-client/build/webpack.base.conf.js
@@ -12,16 +12,8 @@ module.exports = {
 		filename: '[name].js'
 	},
 	resolve: {
-		extensions: ['', '.js', '.vue'],
-		fallback: [path.join(__dirname, '../node_modules')],
-		alias: {
-			'src': path.resolve(__dirname, '../src'),
-			'assets': path.resolve(__dirname, '../src/assets'),
-			'components': path.resolve(__dirname, '../src/components')
-		}
-	},
-	resolveLoader: {
-		fallback: [path.join(__dirname, '../node_modules')]
+		extensions: ['', '.js', '.json', '.vue'],
+		modules: [path.resolve(__dirname, "../src"), path.join(__dirname, '../node_modules')]
 	},
 	module: {
 		loaders: [
