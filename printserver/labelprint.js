5ca1170f346ce39f7d1794cfa88ccd447fa7ddda
rashfael <rashfael@isobeef.org>
2018-05-31 17:08:39 +0200

    change label coors

--- printserver/labelprint.js
+++ printserver/labelprint.js
@@ -6,9 +6,9 @@ module.exports = {
 	async print (product, cb) {
 		let content = 'ZT\nD15\nS0\nN\n'
 		content += `A330,10,0,3,1,1,N,${product.printerline1}\n`
-		content += `A330,45,0,3,1,1,N,${product.printerline2}\n`
-		content += `A330,88,0,3,1,1,N,${product.price.toFixed(2).replace('.', ',')}\n`
-		content += `B600,10,0,3,2,4,75,B,${product.id}\n`
+		content += `A330,50,0,3,1,1,N,${product.printerline2}\n`
+		content += `A330,93,0,3,1,1,N,${product.price.toFixed(2).replace('.', ',')}\n`
+		content += `B600,15,0,3,2,4,75,B,${product.id}\n`
 		content += `P1\n`
 
 		await lp.print(iconv.encode(content, 'ascii'), 'labelprinter')
